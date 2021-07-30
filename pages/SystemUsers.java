1c41b5aa6d370593d44d1b1a21c2825eededdacb
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-07-30 15:54:04 +0530

    [Vinayak] Add: SystemUsers page is added

--- pages/SystemUsers.java
+++ pages/SystemUsers.java
@@ -4,13 +4,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class SystemUsers {
-	
 	@FindBy(xpath="//*[@id=\"menu_admin_viewAdminModule\"]/b")
 	WebElement adminTag;
 	
 	public void clickOnAdminTab() {
 		adminTag.click();
 	}
-	
-
 }
